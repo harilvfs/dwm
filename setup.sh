7a9220df597d36c251fd760d657f6bcc38c1a4fb
Aayush Chalise <aayushchalise8@gmail.com>
2024-09-22 22:20:31 +0545

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,67 +1,17 @@
-#!/bin/bash
-
-echo -ne "
-
-                    ██████╗ ██╗    ██╗███╗   ███╗    ███████╗███████╗████████╗██╗   ██╗██████╗ 
-                    ██╔══██╗██║    ██║████╗ ████║    ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
-                    ██║  ██║██║ █╗ ██║██╔████╔██║    ███████╗█████╗     ██║   ██║   ██║██████╔╝
-                    ██║  ██║██║███╗██║██║╚██╔╝██║    ╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
-                    ██████╔╝╚███╔███╔╝██║ ╚═╝ ██║    ███████║███████╗   ██║   ╚██████╔╝██║     
-                    ╚═════╝  ╚══╝╚══╝ ╚═╝     ╚═╝    ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝    
-
----------------------------------------------------------------------------------------------------------------------
-        This setup installs dependencies for DWM. For further installation instructions, see the README.
----------------------------------------------------------------------------------------------------------------------
-                                                                           
-"
-
-# Function to install dependencies for Debian-based distributions
-install_debian() {
-    sudo apt update
-    sudo apt install build-essential git libx11-dev libx11-xcb-dev libxcb-res0-dev libxinerama-dev libxft-dev libimlib2-dev meson ninja-build
-}
-
-# Function to install dependencies for Arch-based distributions
 install_arch() {
     sudo pacman -Syu --noconfirm
-   sudo pacman -S --needed base-devel git libx11 libxcb libxinerama libxft imlib2 meson ninja
+    sudo pacman -S --needed base-devel git libx11 libxcb libxinerama libxft imlib2 meson ninja
 }
 
-# Detect the distribution and install the appropriate packages
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-    echo "Detected distribution ID: $ID"
-    case "$ID" in
-        debian|ubuntu)
-            echo "Detected Debian-based distribution"
-            echo "Installing Dependencies using apt"
-            install_debian
-            ;;
-        arch|manjaro)
-            echo "Detected Arch-based distribution"
-            echo "Installing packages using pacman"
-            install_arch
-            ;;
-        *)
-            echo "Unsupported distribution: $ID"
-            echo "Contents of /etc/os-release:"
-            cat /etc/os-release
-            exit 1
-            ;;
-    esac
-else
-    echo "/etc/os-release not found. Unsupported distribution"
-    exit 1
-fi
+echo "Installing packages using pacman"
+install_arch
 
-# Function to install Meslo Nerd font for dwm and rofi icons to work
 install_nerd_font() {
     FONT_DIR="$HOME/.local/share/fonts"
     FONT_ZIP="$FONT_DIR/Meslo.zip"
     FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Meslo.zip"
     FONT_INSTALLED=$(fc-list | grep -i "Meslo")
 
-    # Check if Meslo Nerd-font is already installed
     if [ -n "$FONT_INSTALLED" ]; then
         echo "Meslo Nerd-fonts are already installed."
         return 0
@@ -69,44 +19,32 @@ install_nerd_font() {
 
     echo "Installing Meslo Nerd-fonts"
 
-    # Create the fonts directory if it doesn't exist
     if [ ! -d "$FONT_DIR" ]; then
         mkdir -p "$FONT_DIR" || {
             echo "Failed to create directory: $FONT_DIR"
             return 1
         }
-    else
-        echo "$FONT_DIR exists, skipping creation."
     fi
 
-    # Check if the font zip file already exists
     if [ ! -f "$FONT_ZIP" ]; then
-        # Download the font zip file
         wget -P "$FONT_DIR" "$FONT_URL" || {
             echo "Failed to download Meslo Nerd-fonts from $FONT_URL"
             return 1
         }
-    else
-        echo "Meslo.zip already exists in $FONT_DIR, skipping download."
     fi
 
-    # Unzip the font file if it hasn't been unzipped yet
     if [ ! -d "$FONT_DIR/Meslo" ]; then
         unzip "$FONT_ZIP" -d "$FONT_DIR" || {
             echo "Failed to unzip $FONT_ZIP"
             return 1
         }
-    else
-        echo "Meslo font files already unzipped in $FONT_DIR, skipping unzip."
     fi
 
-    # Remove the zip file
     rm "$FONT_ZIP" || {
         echo "Failed to remove $FONT_ZIP"
         return 1
     }
 
-    # Rebuild the font cache
     fc-cache -fv || {
         echo "Failed to rebuild font cache"
         return 1
@@ -116,49 +54,39 @@ install_nerd_font() {
 }
 
 picom_animations() {
-    # Clone the repository in the home/build directory
     mkdir -p ~/build
     if [ ! -d ~/build/picom ]; then
-        if ! git clone https://github.com/FT-Labs/picom.git ~/build/picom; then
+        git clone https://github.com/FT-Labs/picom.git ~/build/picom || {
             echo "Failed to clone the repository"
             return 1
-        fi
-    else
-        echo "Repository already exists, skipping clone"
+        }
     fi
 
     cd ~/build/picom || { echo "Failed to change directory to picom"; return 1; }
 
-    # Build the project
-    if ! meson setup --buildtype=release build; then
+    meson setup --buildtype=release build || {
         echo "Meson setup failed"
         return 1
-    fi
+    }
 
-    if ! ninja -C build; then
+    ninja -C build || {
         echo "Ninja build failed"
         return 1
-    fi
+    }
 
-    # Install the built binary
-    if ! sudo ninja -C build install; then
+    sudo ninja -C build install || {
         echo "Failed to install the built binary"
         return 1
-    fi
+    }
 
     echo "Picom animations installed successfully"
 }
 
 clone_config_folders() {
-    # Ensure the target directory exists
     [ ! -d ~/.config ] && mkdir -p ~/.config
 
-    # Iterate over all directories in config/*
     for dir in config/*/; do
-        # Extract the directory name
         dir_name=$(basename "$dir")
-
-        # Clone the directory to ~/.config/
         if [ -d "$dir" ]; then
             cp -r "$dir" ~/.config/
             echo "Cloned $dir_name to ~/.config/"
@@ -169,45 +97,28 @@ clone_config_folders() {
 }
 
 configure_backgrounds() {
-    # Set the variable BG_DIR to the path where backgrounds will be stored
     BG_DIR="$HOME/Pictures/backgrounds"
 
-    # Check if the ~/Pictures directory exists
     if [ ! -d "~/Pictures" ]; then
-        # If it doesn't exist, print an error message and return with a status of 1 (indicating failure)
-        echo "Pictures directory does not exist"
         mkdir ~/Pictures
-        echo "Directory was created in Home folder"
+        echo "Pictures directory created in Home folder"
     fi
-    
-    # Check if the backgrounds directory (BG_DIR) exists
+
     if [ ! -d "$BG_DIR" ]; then
-        # If the backgrounds directory doesn't exist, attempt to clone a repository containing backgrounds
-        if ! git clone https://github.com/ChrisTitusTech/nord-background.git ~/Pictures; then
-            # If the git clone command fails, print an error message and return with a status of 1
+        git clone https://github.com/ChrisTitusTech/nord-background.git ~/Pictures || {
             echo "Failed to clone the repository"
             return 1
-        fi
-        # Rename the cloned directory to 'backgrounds'
+        }
         mv ~/Pictures/nord-background ~/Pictures/backgrounds
-        # Print a success message indicating that the backgrounds have been downloaded
-        echo "Downloaded desktop backgrounds to $BG_DIR"    
+        echo "Downloaded desktop backgrounds to $BG_DIR"
     else
-        # If the backgrounds directory already exists, print a message indicating that the download is being skipped
-        echo "Path $BG_DIR exists for desktop backgrounds, skipping download of backgrounds"
+        echo "Path $BG_DIR exists, skipping download of backgrounds"
     fi
 }
 
-# Call the function
 install_nerd_font
-
-# Call the function
 clone_config_folders
-
-# Call the function
 picom_animations
-
-# Call the function
 configure_backgrounds
 
 echo "All dependencies installed successfully."
