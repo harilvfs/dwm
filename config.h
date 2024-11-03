3b6b2781ffb7befdaf8a642cf2e4d391f2e39eb7
Aayush Chalise <ingoprivate@gmail.com>
2024-11-03 13:44:03 +0545

    filemanager changed to thunar

--- config.h
+++ config.h
@@ -108,7 +108,7 @@ static Key keys[] = {
     { MODKEY,                       XK_p,          spawn,                  SHCMD ("flameshot full -p ~/Pictures/Screenshots/")}, // capture full screen screenshot
     { MODKEY|ShiftMask,             XK_p,          spawn,                  SHCMD ("flameshot gui -p ~/Pictures/Screenshots/")}, // open flameshot gui for screenshot selection
     { MODKEY|ControlMask,           XK_p,          spawn,                  SHCMD ("flameshot gui --clipboard")}, // copy screenshot to clipboard
-    { MODKEY,                       XK_e,          spawn,                  SHCMD ("nemo")}, // open thunar file manager
+    { MODKEY,                       XK_e,          spawn,                  SHCMD ("thunar")}, // open thunar file manager
     { MODKEY,                       XK_w,          spawn,                  SHCMD ("looking-glass-client -F")}, // start Looking glass
     { MODKEY|ShiftMask,             XK_w,          spawn,                  SHCMD ("feh --randomize --bg-fill ~/Downloads/nord-background//*")}, // start Looking glass
     { MODKEY|ShiftMask,                       XK_i,          spawn,                  SHCMD ("brightnessctl s +10%")}, // increase backlight brightness
