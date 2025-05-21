50f2c9ca78fea4a1bb4bf45cc630e249ab5b151d
Hari Chalise <ingoprivate@gmail.com>
2025-05-22 01:12:54 +0545

    feat: adapt with my profile [ fish ]

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,4 +6,51 @@ if status is-interactive
     alias vim 'nvim'
     alias ll 'ls -la'
     alias la 'ls -la'
+
+
+# --- Exports ---
+set -x PATH $HOME/.cargo/bin $BUN_INSTALL/bin $PATH
+set -x BUN_INSTALL $HOME/.bun
+set -x EDITOR nvim
+
+# --- Aliases ---
+alias c 'clear'
+alias l 'eza -lh --icons=auto'
+alias ls 'eza -1 --icons=auto'
+alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
+alias ld 'eza -lhD --icons=auto'
+alias lt 'eza --icons=auto --tree'
+
+alias un '$aurhelper -Rns'
+alias up '$aurhelper -Syu'
+alias pl '$aurhelper -Qs'
+alias pa '$aurhelper -Ss'
+alias pc '$aurhelper -Sc'
+alias po '$aurhelper -Qtdq | $aurhelper -Rns -'
+
+alias vc 'code'
+alias vim 'nvim'
+alias aa 'startx'
+
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .3 'cd ../../..'
+alias .4 'cd ../../../..'
+alias .5 'cd ../../../../..'
+
+alias mkdir 'mkdir -p'
+
+function cdf
+    set dir (find . -maxdepth 3 -type d | fzf)
+    if test -n "$dir"
+        cd "$dir"
+    end
+end
+
+function lazyg
+    git add .
+    git commit -m "$argv"
+    git push
+end
+
 end
