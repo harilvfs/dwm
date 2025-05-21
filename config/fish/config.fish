5c5305e0a3d985d5ca733e64da532044e2247c8c
Hari Chalise <ingoprivate@gmail.com>
2025-05-22 01:02:44 +0545

    fix: shell alias

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+
+    zoxide init fish | source
+
+    alias vim 'nvim'
+    alias ll 'ls -la'
+    alias la 'ls -la'
 end
-
-zoxide init fish | source
-
-alias vim=nvim
