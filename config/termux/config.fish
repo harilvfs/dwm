726eea4ac5f84c12cbc9ccfa74f0efdf3c8ba5d0
Hari Chalise <ingoprivate@gmail.com>
2026-07-01 13:54:41 +0545

    chore: add fish config for termux

---
+++ config/termux/config.fish
@@ -0,0 +1,73 @@
+if status is-interactive
+
+    set -g fish_greeting
+    # Commands to run in interactive sessions can go here
+
+    zoxide init fish | source
+
+    alias vim 'nvim'
+    alias vi 'nvim'
+    alias ll 'ls -la'
+    alias la 'ls -la'
+    alias e 'exit'
+
+# --- Exports ---
+set -x PATH $HOME/.cargo/bin $BUN_INSTALL/bin $PATH
+set -x BUN_INSTALL $HOME/.bun
+set -x EDITOR nvim
+
+# --- Aliases ---
+alias c 'clear'
+alias ts 'ssh -p 8022 u0_a585@192.168.1.5'  # SSH into Termux
+alias sl ''
+alias l 'eza -lh --icons=auto'
+alias ls 'eza -1 --icons=auto'
+alias ll 'eza -lha --icons=auto --sort=name --group-directories-first'
+alias ld 'eza -lhD --icons=auto'
+alias lt 'eza --icons=auto --tree'
+
+alias un '$aurhelper -Rns'
+alias up '$aurhelper -Syu'
+alias pl '$aurhelper -Qs'
+alias pa '$aurhelper -Ss'
+alias pc '$aurhelper -Sc'
+alias po '$aurhelper -Qtdq | $aurhelper -Rns -'
+
+alias vc 'code'
+alias vim 'nvim'
+alias aa 'startx'
+alias rm 'trash -v'
+alias hx 'helix'
+alias ff 'fastfetch'
+
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .3 'cd ../../..'
+alias .4 'cd ../../../..'
+alias .5 'cd ../../../../..'
+
+alias mkdir 'mkdir -p'
+
+# if you wanna add github token
+
+# set -x GITHUB_TOKEN 
+
+function yayf
+  yay -Slq | fzf --multi --preview 'yay -Sii {1}' --preview-window=down:75% | xargs -ro yay -S
+end
+
+function lazyg
+    git add .
+    git commit -m "$argv"
+    git push
+end
+
+end
+
+set -gx PATH $HOME/.local/bin $PATH
+
+pfetch
+
+function fish_greeting
+   npltz show
+end
